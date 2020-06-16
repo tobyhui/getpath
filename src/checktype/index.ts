b6ed4c9bc73b8aff279737255be3d1439e2d9c91
Toby_hui <>
2020-06-16 14:29:53 +0800

    add typeNameMap entrance to checktype function

--- src/checktype/index.ts
+++ src/checktype/index.ts
@@ -2,7 +2,7 @@
  * @Author: Toby
  * @Date: 2020-06-16 10:57:37
  * @LastEditors: Toby
- * @LastEditTime: 2020-06-16 14:21:50
+ * @LastEditTime: 2020-06-16 14:26:59
  * @FilePath: /getpath/src/checktype/index.ts
  */
 
@@ -13,22 +13,22 @@ import { getFullTypeName } from '../utils/abbr_types';
  * @param data 
  * @param expectType 
  */
-function checkType(data: any, expectType: string): boolean {
+function checkType(data: any, expectType: string, typeNameMap?: { [key: string]: string[] }): boolean {
 	if (arguments.length < 2) return false;
 
-	const typeString: string = getFullTypeName(expectType);
+	const typeString: string = getFullTypeName(expectType, typeNameMap);
 	const typeName: string = getType(data);
 
 	return typeName === typeString;
 }
 
 /**
- * get target type by [Object.prototype.toString.call]
+ * get target type string name by [Object.prototype.toString.call]
  * @param data 
  * @returns 
  *  LowerCase String for type name
  * 
- *  eg. string | array | object | boolean | number | function | math | undefined | null | regexp | symbol | json | window | htmldocument | location | error | arguments | map | set | weakmap | weakset
+ *  eg. string | array | object |Optimize directory structure boolean | number | function | math | undefined | null | regexp | symbol | json | window | htmldocument | location | error | arguments | map | set | weakmap | weakset
  */
 const getType = (data: any): string => {
 	// The method returns a string representing the object.
